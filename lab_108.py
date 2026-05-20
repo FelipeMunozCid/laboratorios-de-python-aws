b52e637b38779d9d49d540c2db25fc687465efa8
felipe <felipe.munoz.cid19@gmail.com>
2026-05-20 15:35:18 -0400

    estos son los lab de la clase de python

---
+++ lab_108.py
@@ -0,0 +1,36 @@
+
+
+print("Hola Mundo")
+
+
+nombre=input("Ingrese nombre: ")
+
+ciudad=input("Ingrese ciudad: ")
+comida=input("Ingrese comida favorita: ")
+print("Hola",nombre)
+print("vives en ", ciudad)
+print("Tu comida favorita es ", comida)
+
+print("Hola",nombre)
+
+print("Eres de",ciudad)
+print("tu comida favorita es",comida)
+
+##### construyendo un perfil
+#"Ahora crearemos un pequeño perfil digital."
+nombre=input("Ingrese nombre: ")
+
+ciudad=input("Ingrese ciudad: ")
+
+edad=input("Ingrese edad: ")
+
+print("Hola",nombre)
+
+print("Vives en",ciudad)
+print("Tienes",edad,"años")
+
+print("Hola",nombre)
+
+print("Tienes",edad,"años y vives en",ciudad)
+
+print("Bienvenido al laboratorio Python")
---
+++ lab_109.py
@@ -0,0 +1,7 @@
+edad=int(input("ingrese edad:"))
+meses = edad*12
+dias = edad*12*30
+horas = edad*12*30*24
+print("usted tiene ", meses, "meses")
+print("usted tiene ", dias, "dias")
+print("usted tiene ", horas, "horas")
